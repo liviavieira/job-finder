b47ba1972aed945a4c8bbc09837e46ebb85db346
liviavieira <vieira.liviaaa@gmail.com>
2022-03-31 21:42:21 -0300

    create vacancy search and profile

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,6 +7,23 @@ router.get('/test', (req, res) => {
   res.send('deu certo');
 });
 
+// vacancy details
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: {id: req.params.id}
+  })
+    .then(job => {
+      res.render('view', {
+        job
+      });
+    })
+    .catch(err => console.log(err))
+);
+
+// sending route form
+router.get('/add', (req, res) => {
+  res.render('add');
+});
+
 // add job
 router.post('/add', (req, res) => {
   let { title, salary, company, description, email, new_job } = req.body;
